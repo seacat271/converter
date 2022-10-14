0cef515888e0fc6fbbc8e155ff635762e4622284
seacat271 <seacat271083@gmail.com>
2022-10-14 16:36:32 +0300

    add markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const API_KEY = "3ZAJRdLdELwDgAdIk1W7T3DHNM3aSmoS"
+
+
 export const Header = () => {
+    const {currency, setCurrency} = useState({});
+
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "3ZAJRdLdELwDgAdIk1W7T3DHNM3aSmoS");
+    
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: myHeaders
+    };
+
+    
+
+
+
+    useEffect(() => {
+        // fetch("https://api.apilayer.com/currency_data/live?source=UAH&currencies=USD,EUR", requestOptions)
+        // .then(response => response.text())
+        // .then(result => console.log(result))
+        // .catch(error => console.log('error', error));
+    })
+
+       
+        
+
     return (
-        <div></div>
+        <header>
+            <div>
+                <div>
+                    <span>SVG</span>
+                    <span>BEST CURRENCY</span>
+                </div>
+          <div>
+            <div>
+                    <span>0.00</span>
+                    <span>USD</span>
+                </div>
+                <div>
+                    <span>0.00</span>
+                    <span>EUR</span>
+                </div>
+            </div>
+            </div>
+        </header>
     )
 }
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,5 +1,30 @@
 export const MainBody = () => {
     return (
-        <div></div>
+        <main>
+            <div> 
+                <div>
+                    <form>
+                    <input type="number" placeholder="Enter quantity"/>
+                    <select name="currency1" size="1">
+                        <option value="uah" selected="selected">UAH</option>
+                        <option value="usd">USD</option>
+                        <option value="eur">EUR</option>
+                    </select>
+                    </form>
+                </div>
+                {/* <button>Convert</button> */}
+                <div>
+                    <form>
+                    <input type="number"  placeholder="Enter quantity"/>
+                    <select name="currency2" size="1">
+                        <option value="uah" selected="selected">UAH</option>
+                        <option value="usd">USD</option>
+                        <option value="eur#">EUR</option>
+                    </select>
+                    </form>
+                </div>
+            </div>
+
+        </main>
     )
 }
