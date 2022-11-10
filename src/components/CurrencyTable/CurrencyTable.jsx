06dce4abb18ad11161946be9e381e03355f4571a
seacat271 <seacat271083@gmail.com>
2022-11-10 12:23:21 +0200

    Update CurrencyTable.jsx

--- src/components/CurrencyTable/CurrencyTable.jsx
+++ src/components/CurrencyTable/CurrencyTable.jsx
@@ -1,9 +1,9 @@
-import { Loader } from "../../helpers/Loader";
-import { SpanTitle } from "../AppBar/AppBar.styled";
+// import { Loader } from "../../helpers/Loader";
+// import { SpanTitle } from "../AppBar/AppBar.styled";
 
-export const CurrencyTable = ({value}) => {
-    const currentValue = (1/value).toFixed(4)
-    return (
-    <SpanTitle>{value ? currentValue : <Loader/>}</SpanTitle>
-    )
-}
+// export const CurrencyTable = ({value}) => {
+//     const currentValue = (1/value).toFixed(4)
+//     return (
+//     <SpanTitle>{value ? currentValue : <Loader/>}</SpanTitle>
+//     )
+// }
